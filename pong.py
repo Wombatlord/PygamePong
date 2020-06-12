b2f3875df0b6d744438dbcd3c63b6e6e36ebea6c
Wombatlord <jordan.stone0@gmail.com>
2020-06-12 21:54:03 +0100

    Added basic score & scoreboard. Added Ball.destroy() to Ball to fix position when leaving play space.

--- pong.py
+++ pong.py
@@ -4,15 +4,23 @@ import random
 
 pygame.init()
 
-# Variables
+# Screen Variables
 HEIGHT = 600
 WIDTH = 1200
 BORDER = 20
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Game Object Variables
 fgColour = pygame.Color("red")
 ballColour = pygame.Color("black")
 paddleColour = pygame.Color("blue")
 velocity = 300
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Scoreboard Variables
+scoreBoard = pygame.Surface((300, 100))
+scoreBoardPos = (50, 50)
+scoreBoardSize = (300, 100)
+scoreValue = 0
 
 
 def current_time():
@@ -36,6 +44,22 @@ def renderWalls():
     pygame.draw.rect(screen, fgColour, pygame.Rect((0, 0), (WIDTH, BORDER)))
     pygame.draw.rect(screen, fgColour, pygame.Rect((0, 0), (BORDER, HEIGHT)))
     pygame.draw.rect(screen, fgColour, pygame.Rect((0, HEIGHT - BORDER), (WIDTH, BORDER)))
+
+
+def renderScoreBoard():
+    global screen, scoreBoard, scoreBoardPos
+    scoreBoard = scoreBoard.convert()
+    scoreBoard.fill((255, 255, 255))
+    screen.blit(scoreBoard, scoreBoardPos)
+
+
+def renderScore():
+    global screen, scoreBoard, scoreBoardPos, scoreBoardSize, scoreValue
+    font = pygame.font.Font(None, 36)
+    score = font.render(str(scoreValue), 1, (10, 10, 10))
+    textPos = score.get_rect(centerx=scoreBoardPos[0] + scoreBoardSize[0] // 2,
+                             centery=scoreBoardPos[1] + scoreBoardSize[1] // 2)
+    screen.blit(score, textPos)
 
 
 class Paddle:
@@ -89,7 +113,12 @@ class Ball:
         global screen
         pygame.draw.circle(screen, ballColour, (int(self.x), int(self.y)), Ball.RADIUS)
 
+    def destroy(self):
+        global ballPlay
+        ballPlay = Ball(WIDTH - Ball.RADIUS, HEIGHT // 2, 0, 0)
+
     def update(self, paddle: Paddle):
+        global scoreValue, WIDTH, HEIGHT, velocity
         now = current_time()
 
         if self.timeOfLastUpdate is None:
@@ -104,16 +133,22 @@ class Ball:
 
         hasCollided = self.getHitBox().colliderect(paddle.getHitBox())
         horizontalOutOfBounds = newX < BORDER + Ball.RADIUS
+        offScreen = newX > WIDTH
+
+        if offScreen:
+            Ball.destroy(self)
 
         if horizontalOutOfBounds:
-            self.vx = -self.vx + random.randint(25,100)
+            scoreValue += 1
+            self.vx = -self.vx + random.randint(25, 100)
             newX = self.x + timeSinceLastUpdate * self.vx
 
         if hasCollided:
-            self.vx = -abs(self.vx)
-            self.vy = -abs(self.vy) + random.randint(200, 350)
+            self.vx = -abs(self.vx) + random.randint(-25, 25)
+            self.vy = -abs(self.vy) + random.randint(-200, 350)
 
         if newY < BORDER + Ball.RADIUS or newY > HEIGHT - BORDER - Ball.RADIUS:
+            scoreValue += 1
             self.vy = -self.vy
             newY = self.y + timeSinceLastUpdate * self.vy
 
@@ -129,12 +164,13 @@ while True:
     if e.type == pygame.QUIT:
         break
 
-
+    renderBackground()
+    renderScoreBoard()
+    renderScore()
+    renderWalls()
     paddlePlay.show()
     ballPlay.show()
     pygame.display.flip()
-    renderBackground()
-    renderWalls()
     ballPlay.update(paddlePlay)
     paddlePlay.update()
 
